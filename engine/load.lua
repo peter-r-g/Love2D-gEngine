9d2f3a7f881b72d149a7529200075490c99e82fa
Peter <petergorman11@gmail.com>
2019-03-05 18:02:42 -0500

    Multiple additions/fixes, check commit description
    Fixed gEngine loading.
    Add module system.
    Add advanced require implementation.

--- engine/load.lua
+++ engine/load.lua
@@ -1,31 +1,70 @@
-_G.Class = require("engine.vendor.middleClass")
-_G.gEngine = Class("gEngine")
-_G.gEngine.static.Config = require("engine.config")
+local this = ...
 
-function gEngine:Load()
-    require("engine.utility.table")
+require(string.gsub(this, "load$", "") .. "vendor.require")
+
+_G.Class = require.relative(this, "vendor.middleClass")
+local ENGINE_CLASS = Class("gEngine")
+
+function ENGINE_CLASS:initialize()
+    self.enabledModules = {}
+    self.Config = require.relative(this, "config")
+    
+    require.relative(this, "utility.table")
 
     if self.Config.useSteam then
-        require("luasteam")
+        self.Steam = require("luasteam")
+        self:EnableModule("Steam")
     end
 
     if self.Config.useMultiplayer then
-        require("engine.vendor.bitser")
-        require("engine.vendor.sock")
+        require.relative(this, "vendor.bitser")
+        require.relative(this, "vendor.sock")
+        self:EnableModule("Multiplayer")
     end
 
-    self.Color = require("engine.type.color")
-    self.Vector2 = require("engine.type.vector2")
+    self.Color = require.relative(this, "type.color")
+    self.Vector2 = require.relative(this, "type.vector2")
 end
 
-function gEngine:Update(dt)
-
+function ENGINE_CLASS:Load()
+    if self:IsModuleEnabled("Steam") then
+        self.Steam.init()
+    end
 end
 
-function gEngine:Draw()
-
+function ENGINE_CLASS:Update(dt)
+    if self:IsModuleEnabled("Steam") then
+        self.Steam.runCallbacks()
+    end
 end
 
-function gEngine:Quit()
+function ENGINE_CLASS:Draw() end
 
+function ENGINE_CLASS:Quit()
+    local shouldQuit = self:OnQuit()
+    if shouldQuit then return true end
+    
+    if self:IsModuleEnabled("Steam") then
+        self.Steam.shutdown()
+    end
 end
+
+function ENGINE_CLASS:EnableModule(moduleName)
+    self.enabledModules[moduleName] = true
+    self:OnModuleEnabled(moduleName)
+end
+
+function ENGINE_CLASS:DisableModule(moduleName)
+    self.enabledModules[moduleName] = false
+    self:OnModuleDisabled(moduleName)
+end
+
+function ENGINE_CLASS:IsModuleEnabled(moduleName)
+    return self.enabledModules[moduleName] or false
+end
+
+function ENGINE_CLASS:OnQuit() end
+function ENGINE_CLASS:OnModuleEnabled(moduleName) end
+function ENGINE_CLASS:OnModuleDisabled(moduleName) end
+
+return ENGINE_CLASS()
