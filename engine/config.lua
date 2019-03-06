5b2326cb6e5ab383ce63a5d5e5770586ce0321d2
Peter <petergorman11@gmail.com>
2019-03-05 19:47:36 -0500

    Add config options for modules and messages

--- engine/config.lua
+++ engine/config.lua
@@ -1,6 +1,14 @@
 local CONFIG = {}
 
+CONFIG.useWarnings = false
+CONFIG.useErrors = true
+
+CONFIG.useColor = true
+CONFIG.useVector2 = true
 CONFIG.useMultiplayer = false
 CONFIG.useSteam = false
+CONFIG.useEvent = true
+CONFIG.useTimer = true
+CONFIG.useInput = true
 
 return CONFIG
