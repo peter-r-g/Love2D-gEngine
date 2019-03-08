abf04c70cf575188d2128f9b219e96f4680e9eac
Peter <petergorman11@gmail.com>
2019-03-08 18:50:01 -0500

    Remove configuration for disabling/enabling most modules

--- engine/config.lua
+++ engine/config.lua
@@ -2,13 +2,7 @@ local CONFIG = {}
 
 CONFIG.useWarnings = false
 
-CONFIG.useColor = true
-CONFIG.useVector2 = true
 CONFIG.useMultiplayer = false
 CONFIG.useSteam = false
-CONFIG.useEvent = true
-CONFIG.useTimer = true
-CONFIG.useInput = true
-CONFIG.useScene = true
 
 return CONFIG
