07f9e323f65ac301f6080763ab11114329b277b5
Peter <petergorman11@gmail.com>
2019-03-08 22:47:23 -0500

    Add basic entity with 2D position

---
+++ engine/entities/positioned-entity.lua
@@ -0,0 +1,23 @@
+local ENTITY = Class("Entity-Positioned", require.relative(..., "base"))
+
+function ENTITY:OnEntityCreated(pos)
+    self.pos = pos or gEngine.Vector2(0, 0)
+end
+
+function ENTITY:SetPos(pos)
+    self.pos = pos
+    
+    for childId, child in pairs(self.children) do
+        child:OnParentPosChanged(pos)
+    end
+end
+
+function ENTITY:GetPos()
+    return self.pos
+end
+
+function ENTITY:OnParentPosChanged(pos)
+    self:SetPos(self.pos + pos)
+end
+
+return ENTITY
